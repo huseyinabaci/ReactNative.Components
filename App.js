5684ec3095acff04222dc41c1354a73c8c196da5
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2024-06-30 17:06:30 +0300

    Components 2

--- App.js
+++ App.js
@@ -1,28 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const message = 'Javascript';
+  const courses = [
+    { name: "Angular", id: 1 },
+    { name: "React Js", id: 2 },
+    { name: "C#", id: 3 },
+    { name: "C Programlama", id: 4 },
+    { name: "Bootstrap", id: 5 },
+  ];
   return (
-    <View style={styles.container}>
-        <Text style={styles.title}>React Native</Text>
-        <Text style={styles.content}>React Js</Text>
-        <Text style={styles.content}>{message}</Text>
-    </View>
+    <FlatList
+      // horizontal={true}
+      // showsHorizontalScrollIndicator={false}
+      data={courses}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => {
+        return <Text style={styles.content}>{item.name}</Text>;
+      }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title:{
-    fontSize:50,
-  },
-  content:{
-    fontSize:20,
+  content: {
+    fontSize: 20,
+    backgroundColor: 'yellow',
+    marginVertical: 10,
+    padding: 20,
   },
 });
+
